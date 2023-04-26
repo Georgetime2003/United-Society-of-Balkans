5ae35809b15c9259c69dd4b75d0a0b18fb75cef2
Jordi Palomino i Escarrà <familiajordiescarra@gmail.com>
2023-04-26 10:37:55 +0300

    Update Weekly Reports

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -12,7 +12,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule): void
     {
-        // $schedule->command('inspire')->hourly();
+        $schedule->call([\App\Http\Controllers\Reports::class, 'schedule'])->weekly();
     }
 
     /**
---
+++ app/Http/Controllers/Reports.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class Reports extends Controller
+{
+    public function schedule(){
+        
+    }
+}
