c5dcd089a419086ff5848ab9bb76a742c615bff4
Jordi Palomino i Escarrà <familiajordiescarra@gmail.com>
2023-05-03 16:30:36 +0300

    change css stuff

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,5 +1,59 @@
-@extends('header')
-@section('content')
+<!DOCTYPE html>
+<html lang="ca-es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>USB</title>
+    <link type="text/css" rel="stylesheet" href="{{ asset('css/bootstrap.min.css') }}">
+    <link type="text/css" rel="stylesheet" href="{{ asset('css/login.css') }}">
+    <script src="{{ asset ('js/jquery.min.js')}}"></script>
+    <script src="{{ asset('js/bootstrap.min.js')}}"></script>
+    <link type="text/css" rel="stylesheet" href="{{ asset('fontawesome/css/brands.css')}}" rel="stylesheet">
+    <link type="text/css" rel="stylesheet" href="{{ asset('fontawesome/css/solid.css')}}" rel="stylesheet">
+    <link type="text/css" rel="stylesheet" href="{{ asset('fontawesome/css/fontawesome.css')}}" rel="stylesheet">
+    <meta name="mobile-web-app-capable" content="yes">
+    <meta name="apple-mobile-web-app-capable" content="yes">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <script src="{{ asset('js/jquery.min.js') }}"></script>
+    <link rel="icon" sizes="960x960" href="{{ asset('images/logo.png') }}">
+    <link rel="icon" type="image/png" sizes="180x180" href="{{ asset('images/apple-touch-icon.png') }}">
+<script>
+        if (navigator.userAgent.match(/Android/i)) {
+            window.scrollTo(0,0); // reset in case prev not scrolled  
+            var nPageH = $(document).height();
+            var nViewH = window.outerHeight;
+            if (nViewH > nPageH) {
+              nViewH -= 250;
+              $('BODY').css('height',nViewH + 'px');
+            }
+            window.scrollTo(0,1);
+        } else if (navigator.userAgent.match(/iPhone/i)) {
+            window.scrollTo(0,0); // reset in case prev not scrolled  
+            var nPageH = $(document).height();
+            var nViewH = window.outerHeight;
+            if (nViewH > nPageH) {
+              nViewH -= 250;
+              $('BODY').css('height',nViewH + 'px');
+            }
+            window.scrollTo(0,1);
+        } else if (navigator.userAgent.match(/iPad/i)) {
+            window.scrollTo(0,0); // reset in case prev not scrolled  
+            var nPageH = $(document).height();
+            var nViewH = window.outerHeight;
+            if (nViewH > nPageH) {
+              nViewH -= 250;
+              $('BODY').css('height',nViewH + 'px');
+            }
+            window.scrollTo(0,1);
+        }
+    </script>
+    @yield('header')
+</head>
+<body>
+    @yield('content')
+</body>
+</html>
 <main class="bg-image">
         <div class="container">
             <div class="row">
@@ -34,7 +88,6 @@
             </div>
         </div>
 </main>
-@endsection
 
 {{-- @extends('layout')
 {{--     <form action="{{route('participacions')}}" method="POST" enctype="multipart/form-data">
--- routes/web(with middleware).php
+++ routes/web(with middleware).php
@@ -21,16 +21,16 @@ Route::get('/callback/google', [App\Http\Controllers\login::class, 'GoogleCallba
 Route::get('/callback/facebook', [App\Http\Controllers\login::class, 'FacebookCallback'])->name('facebook.callback');
 Route::get('callback/microsoft', [App\Http\Controllers\login::class, 'MicrosoftCallback'])->name('microsoft.callback');
 
-Route::get('/users', [App\Http\Controllers\Users::class, 'index'])->name('users');
-Route::get('/user/config', [App\Http\Controllers\Users::class, 'config'])->name('users.config');
-Route::post('/user/delete/{id}', [App\Http\Controllers\Users::class, 'delete'])->name('users.delete');
-Route::get('/user/{id}', [App\Http\Controllers\Users::class, 'show'])->name('users.show');
-Route::post('/user', [App\Http\Controllers\Users::class, 'update'])->name('users.update');
+Route::get('/users', [App\Http\Controllers\Users::class, 'index'])->name('users')->middleware('isAdmin');
+Route::get('/user/config', [App\Http\Controllers\Users::class, 'config'])->name('users.config')->middleware('isVolunteer');
+Route::post('/user/delete/{id}', [App\Http\Controllers\Users::class, 'delete'])->name('users.delete')->middleware('isAdmin');
+Route::get('/user/{id}', [App\Http\Controllers\Users::class, 'show'])->name('users.show')->middleware('isAdmin');
+Route::post('/user', [App\Http\Controllers\Users::class, 'update'])->name('users.update')->middleware('isAdmin');
 
-Route::get('/reports', [App\Http\Controllers\Reports::class, 'index'])->name('reports');
-Route::get('/reports/{userid}', [App\Http\Controllers\Reports::class, 'listUser'])->name('reports.userList');
-Route::get('/reports/{userid}/{reportid}', [App\Http\Controllers\Reports::class, 'show'])->name('reports.show');
-Route::post('/weeklyreport/update', [App\Http\Controllers\Reports::class, 'updateweekly'])->name('reports.updateweekly');
+Route::get('/reports', [App\Http\Controllers\Reports::class, 'index'])->name('reports')->middleware('isAdmin');
+Route::get('/reports/{userid}', [App\Http\Controllers\Reports::class, 'listUser'])->name('reports.userList')->middleware('isAdminorVolunteer');
+Route::get('/reports/{userid}/{reportid}', [App\Http\Controllers\Reports::class, 'show'])->name('reports.show')->middleware('isAdminorVolunteer');
+Route::post('/weeklyreport/update', [App\Http\Controllers\Reports::class, 'updateweekly'])->name('reports.updateweekly')->middleware('isVolunteer');
 
 //Temporal home routing
 Route::get('/home', function () {
