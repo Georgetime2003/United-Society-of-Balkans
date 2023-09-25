e8f36a4a55a24eef298574305457c36941e91b03
Georgetime2003 <familiajordiescarra@gmail.com>
2023-09-25 19:01:50 +0300

    Change design

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -6,24 +6,24 @@
 		<img src="/images/logonav.png" alt="Logo" width="40" height="38" class="d-inline-block">
 	  </a>
 	  <div class="d-flex">
-		<a class="btn btn-outline-light me-2" href="/home"><i class="fas fa-home" style="width: 20px; margin-top: 20%"></i></a>
+		<a class="btn linkmenubar me-2" href="/home"><i class="fas fa-home" style="width: 20px; margin-top: 20%"></i></a>
 		@if (Auth::user())
+			<a class="btn linkmenubar me-2" href="/user/config"><i class="fas fa-user" style="width: 20px; margin-top: 20%"></i></a>
 			@if (Auth::user()->role == 'admin' || Auth::user()->role == 'superadmin')
-			<a class="btn btn-outline-light me-2" href="/users">Users</a>
-			<a class="btn btn-outline-light me-2" href="/forum">Forum</a>
-			<a class="btn btn-outline-light me-2" href="/reports">Reports</a>
-			<a class="btn btn-outline-light me-2" href="/organizations">Organizations</a>
+			<a class="btn linkmenubar me-2" href="/users">Users</a>
+			<a class="btn linkmenubar me-2" href="/forum">Forum</a>
+			<a class="btn linkmenubar me-2" href="/reports">Reports</a>
+			<a class="btn linkmenubar me-2" href="/organizations">Organizations</a>
 			@elseif (Auth::user()->role == 'volunteer')
-				<a class="btn btn-outline-light me-2" href="/reports/{{Auth::user()->id}}">Reports</a>
-				<a class="btn btn-outline-light me-2" href="/forum">Forum</a>
-				<a class="btn btn-outline-light me-2" href="/user/config">Account</a>
+				<a class="btn linkmenubar me-2" href="/reports/{{Auth::user()->id}}">Reports</a>
+				<a class="btn linkmenubar me-2" href="/forum">Forum</a>
 			@elseif (Auth::user()->role == 'housemanager')
-				<a class="btn btn-outline-light me-2" href="/forum">Forum</a>
+				<a class="btn linkmenubar me-2" href="/forum">Forum</a>
 			@elseif (Auth::user()->role == 'organization')
-				<a class="btn btn-outline-light me-2" href="/organization/{{Auth::user()->id}}">Reports</a>
+				<a class="btn linkmenubar me-2" href="/organization/{{Auth::user()->id}}">Reports</a>
 			@endif
 			@endif
-			<a class="btn btn-danger me-2" href="/logout" onclick="logout(event)">Logout</a>
+			<a class="btn linkmenulogout me-2" href="/logout" onclick="logout(event)"><i class="fa-solid fa-arrow-right-from-bracket" style="margin-top: 20%; margin-left: 8%"></i></a>
 	  </div>
 	</div>
 </nav>
