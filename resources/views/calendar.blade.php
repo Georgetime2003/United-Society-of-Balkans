8a3d459d24e8773d140b6cc2d29b21dd2dd09bec
Jordi Escarrà <familiajordiescarra@gmail.com>
2024-05-02 16:13:21 +0200

    Calendar added migrate + Main menu

--- resources/views/calendar.blade.php
+++ resources/views/calendar.blade.php
@@ -1,13 +1,5 @@
-<!DOCTYPE html>
-<html lang="ca-es">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>USB</title>
-    <link type="text/css" rel="stylesheet" href="{{ asset('css/bootstrap/bootstrap.min.css') }}">
-    <script src="{{ asset ('js/jquery.min.js')}}"></script>
-    <script src="{{ asset('js/bootstrap/bootstrap.min.js')}}"></script>
+@extends('layout')
+@section('header')
     <script src="https://cdn.jsdelivr.net/npm/fullcalendar@6.1.11/index.global.min.js"></script>
     <style>
         .dialeg {
@@ -99,10 +91,8 @@ document.addEventListener('DOMContentLoaded', function() {
 
         });
     </script>
-</head>
-<body>
-
-
+@endsection
+@section('site_content')
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
@@ -139,4 +129,4 @@ document.addEventListener('DOMContentLoaded', function() {
             </div>
         </div>
     </div>
-</body>
+@endsection
