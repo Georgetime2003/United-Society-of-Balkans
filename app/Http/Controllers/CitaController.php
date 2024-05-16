bd62f0dfaeff5d01ad615e569d8c1091574a3f1b
BenitoMF <b.martinez2@sapalomera.cat>
2024-05-16 15:38:16 +0300

    Insert all the day in the database

--- app/Http/Controllers/CitaController.php
+++ app/Http/Controllers/CitaController.php
@@ -16,8 +16,8 @@ class CitaController extends Controller
     
         // Agregar las horas automáticamente si es para todo el día
         if ($request->has('allDay')) {
-            $start_date = $request->input('date') . ' 10:10'; // 
-            $end_date = $request->input('date') . ' 12:12'; // Añade la hora de fin (antes de medianoche)
+            $start_date = $request->input('date') . ' 00:00:00'; // 
+            $end_date = $request->input('date') . ' 23:59:59'; // Añade la hora de fin (antes de medianoche)
         } else {
             $start_date = $request->input('start_date');
             $end_date = $request->input('end_date');
